98647ebdc1ed6a9a05a9c9cf3e588f52ed395e16
dentakon <111415672+dentakon@users.noreply.github.com>
2022-09-19 09:11:15 +0200

    Create metadata.php

---
+++ metadata.php
@@ -0,0 +1,31 @@
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'varsearch',
+    'title'       => array(
+        'de' => 'Variantensuche',
+        'en' => 'Varsearch',
+    ),
+    'description' => array(
+        'de' => '<h2>Modul zum Anzeigen von Variantenartikeln bei der Suche</h2>',
+        'en' => '<h2>Module to show variants by article search</h2>',
+    ),
+    'thumbnail'   => 'out/pictures/',
+    'version'     => '1.0.0',
+    'author'      => 'David Hennig | Dentakon dentale Konzepte',
+    'url'         => 'https://www.dentakon.de',
+    'email'       => 'davidhennig@dentakon.de',
+    'extend'      => array(
+    ),
+
+    'controllers' => array(
+    ),
+
+    'templates'   => array(
+    ),
+    'blocks'      => array(
+    ),
+    'settings'    => array(),
+    'events' => array(),
+);
